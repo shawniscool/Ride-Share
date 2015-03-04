4c85a02fc4b6a2e9739bc223faa423b1316c6984
Terry <terry.kusumartin@gmail.com>
2015-03-04 04:31:41 -0500

    index.js progress report #2

--- js/index.js
+++ js/index.js
@@ -1,34 +1,67 @@
 $(document).ready(function(){
-	//get date from click
+	
 	
 	//Parse query
 	Parse.initialize("60pOb4BeNwwtN2Kmge840kLA7flw0GpVWJ1m0fAr", "YugQLv3sJlPvHtuITAW1gmCd85ubEoOePgqlxr5w");
 	var rideObject = Parse.Object.extend("Ride");
-	var query = new Parse.Query("Ride");
-
+	var query = new Parse.Query(rideObject);
+	query.ascending("date");
+	
 	//Find all dates in database
 	var dateList = [];
 	query.find({
 		success:function(results){
 			for(var i = 0; i<results.length; i++){
-				ind = dateList.indexOf(results[i].get("date"))
+				ind = dateList.indexOf(results[i].get("date"));
 				if (ind == -1) {
-					dateList.push(results[i].get("date"))
+					dateList.push(results[i].get("date"));
 				}
 			}
+			console.log("DateList:" + dateList);
+			for(var i = 0; i<dateList.length; i++){
+				console.log("there");
+				var md = parseDate(dateList[i])//returns array of [month, day]
+				var className = md[0].toString() + md[1].toString();
+				var sectionNum="section"+i.toString();
+				$('#content').append("<section id ='" + sectionNum + "' class ='" + className + "'></section>");
+				$('#dates').append("<li><a href = #" + sectionNum + "><span>" + (md[0]+1).toString() + "/" + md[1].toString() + "</span></a></li>");
+
+				
+			}
+			var text = "";
+			for(var i = 0; i<results.length; i++){
+				console.log("here");
+				var ride = results[i];
+				text += "<div class = 'mediabox'>";
+				text += "<h3>" + ride.get("time") + "</h3>";
+				text += "<h5>" + ride.get("direction") + "</h5>";
+				text += "<ul>";
+					text += "<li> Name: " + ride.get("name") + "</li>";
+					text += "<li> Contact Info: </li>";
+					text += "<ul>";
+						text += "<li> Phone: " + ride.get("phone") + "</li>";
+						text += "<li> email: " + ride.get("email") + "</li>";
+					text += "</ul>";
+					text += "<li> Preferred Meeting Place: " + ride.get("place") + "</li>";
+					text += "<li> Method of Transportation" + ride.get("transportation") + "</li>";
+					text += "<li> Additional Comments: " + ride.get("additional") + "</li>";
+				text += "</ul>";
+				var md =  parseDate(ride.get("date"));
+				var className = md[0].toString() + md[1].toString();
+				var classStr = "."+className;
+				$(classStr).append(text);
+				text = "";
+
+			}
+			
 		},
 		error: function(err){
 			alert("Query to database raised unexpected error.")
 		}
 	});
 
-	//TODO: sort the dates
-	for(var i = 0; i<dateList.length; i++){
-		console.log(dateList[i]);
-		sectionNum="section"+i.toString()
-		$('#dates').appendChild("<li><a href = " + sectionNum + "><span>" + dateList[i] + "</span></a></li>");
-		$('#content').appendChild("<section id =" + sectionNum + "></section>");
-	}
+
+	
 
 	/*
 	query.equalTo("date", date);
@@ -61,3 +94,9 @@ $(document).ready(function(){
 		}
 	});*/
 });
+
+function parseDate(d){
+	month = d.getMonth();
+	day = d.getDate();
+	return [month, day];
+}
